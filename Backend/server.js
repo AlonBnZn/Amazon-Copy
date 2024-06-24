ab9d386f9432466b6bacbc61a32044e7b65ac063
DoronBenZaken <alonbnzn@gmail.com>
2024-06-24 09:15:01 +0300

    models added

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,23 @@
 import express from "express";
-//asdasdsadaaasdasd
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import Product from "./models/Product.js";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port  ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
