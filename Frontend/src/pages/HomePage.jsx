8124c1a992d4fc8a5d482cabf39b31dad81c459a
DoronBenZaken <alonbnzn@gmail.com>
2024-06-24 16:41:10 +0300

    BackEnd & FrontEnd functionality

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,51 @@
 import Title from "../components/shared/Title";
+import { useEffect, useReducer } from "react";
+import homePageReducer from "../reducers/homePageReducer.jsx";
+import { GET_FAIL, GET_REQUEST, GET_SUCCESS } from "../actions.jsx";
+import axios from "axios";
+
+const initialState = {
+  loading: true,
+  error: "",
+  Product: [],
+};
+
 const HomePage = () => {
+  const [state, dispatch] = useReducer(homePageReducer, initialState);
+  useEffect(() => {
+    const getProducts = async () => {
+      dispatch({ type: GET_REQUEST });
+      try {
+        const res = await axios.get("http://localhost:8080/api/v1/products");
+        dispatch({ type: GET_SUCCESS, payload: res.data });
+      } catch (error) {
+        dispatch({ type: GET_FAIL, payload: error.message });
+      }
+    };
+    getProducts();
+  }, []);
   return (
-    <div>
+    <>
       <Title title={"HomePage"} />
-      HomePage
-    </div>
+      <div className="backgroundHomePage">
+        <img
+          style={{ width: "100%" }}
+          src="https://m.media-amazon.com/images/I/81d5OrWJAkL._SX3000_.jpg"
+          alt="backgroundHomePage"
+        />
+      </div>
+      <div className="products">
+        {state.loading ? (
+          // <loading />
+          <div>loading</div>
+        ) : state.error ? (
+          // <MassageBox variant="danger">{state.error}</MassageBox>
+          <div>{state.error}</div>
+        ) : (
+          <div>products dispalyed here</div>
+        )}
+      </div>
+    </>
   );
 };
 
